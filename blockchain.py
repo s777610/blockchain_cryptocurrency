cd14d1ad6adf467859d8f7fd903eb962013b122c
WeiChengHung <s777610@gmail.com>
2018-08-28 00:12:16 -0700

    add sender, recipient, amount

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,8 @@
 
 
 blockchain = []
+open_transactions = []  # not been included in a new block yet
+owner = 'wilson'
 
 
 def get_last_blockchain_value():
@@ -10,14 +12,21 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_transaction(transaction_amount, last_transaction=[1]):
-    if last_transaction == None:
-        last_transaction = [1]
-    blockchain.append([last_transaction, transaction_amount])
+def add_transaction(recipient, sender=owner, amount=1.0):
+    transaction = {'sender': sender,
+                   'recipient': recipient,
+                   'amount': amount}
+    open_transactions.append(transaction)
+
+
+def mine_block():
+    pass
 
 
 def get_transaction_value():
-    return float(input("Your transaction amount please: "))
+    tx_recipient = input('Enter the recipient of the transaction: ')
+    tx_amount = float(input("Your transaction amount please: "))
+    return tx_recipient, tx_amount
 
 
 def get_user_choice():
@@ -72,8 +81,10 @@ while waiting_for_input:
     print("q: Quit")
     user_choice = get_user_choice()
     if user_choice == '1':
-        tx_amount = get_transaction_value()
-        add_transaction(tx_amount, get_last_blockchain_value())
+        tx_data = get_transaction_value()
+        recipient, amount = tx_data
+        add_transaction(recipient, amount=amount)
+        print(open_transactions)
     elif user_choice == '2':
         print_blockchain_elements()
     elif user_choice == 'h':
