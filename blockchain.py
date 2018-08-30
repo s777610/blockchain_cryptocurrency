a6b31869e5e51e24ab5ae3d1144620c4fb3bc238
WeiChengHung <s777610@gmail.com>
2018-08-30 02:15:50 -0700

    get balance

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,5 @@
+import functools
+
 # blockchain is list
 # block is dict
 # open_transactions is list of transaction
@@ -28,18 +30,13 @@ def get_balance(participant):
     # amount in open transaction
     open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)  # a list, consist all send transaction(blockchain and open transaction)
+    # print(tx_sender)  # [[], [], [], [], [12.0, 2.0], [9.4], [3.0, 2.1, 1.2], []]
 
-    amount_sent = 0
-    for tx in tx_sender:
-        if len(tx) > 0:
-            amount_sent += tx[0]
-
+    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_sender, 0)
     tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
-    amount_received = 0
-    for tx in tx_recipient:
-        if len(tx) > 0:
-            amount_received += tx[0]
+    amount_received = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_recipient, 0)
 
+    # return total balance
     return amount_received - amount_sent
 
 
@@ -176,7 +173,8 @@ while waiting_for_input:
         print_blockchain_elements()
         print("Invalid Blockchain!")
         break
-    print(get_balance('wilson'))
+    # :6 = 6 digits for it, .2f = only show two decimal places
+    print('Balance of {}: {:6.2f}'.format('wilson', get_balance('wilson')))
 else:
     print('User left!')
 
